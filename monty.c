cd08371911ff21fa816b18bfc820a16061512598
MAHAM <abdulkabore@gmail.com>
2022-06-15 21:15:14 +0200

    add monty.c

---
+++ monty.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * main - the main program
+ *
+ * @argc: the arg count
+ * @argv: the args
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("Nothing here now...\n");
+	}
+}
