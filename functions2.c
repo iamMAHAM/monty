27f7249e25c48931bcd25e6aa83e0590bb930101
MAHAM <abdulkabore@gmail.com>
2022-06-16 23:37:50 +0200

    print nothing

---
+++ functions2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * mnop - Does absolutely nothing for the Monty opcode 'nop'.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void mnop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
